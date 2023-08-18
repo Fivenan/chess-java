0dd504f1420764b9460e2465bbde929c94eba366
Fivenan <74267476+Fivenan@users.noreply.github.com>
2023-08-19 01:15:27 +0200

    add util, and test

--- src/main/java/chess/models/util/FENUtil.java
+++ src/main/java/chess/models/util/FENUtil.java
@@ -5,7 +5,29 @@ import java.util.stream.Collectors;
 
 public class FENUtil {
 
+	public static final String STARTING_POSITION_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+	public static final String DEFAULT_FEN_EMBELLISH_ARGUMENTS = " w - - 0 1";
+
 	private FENUtil() {
+	}
+
+	/**
+	 * If a FEN has a valid 1st argument (piece placement), but one or more other
+	 * arguments are invalid, the default FEN argument will be returned.
+	 * PiecePlacement w - - 0 1
+	 * 
+	 * @param fen
+	 * @return
+	 */
+	public static String refreshFEN(String fen) {
+		String[] ranks = fen.split(" ");
+		if (!NotationValidator.isValidFENPiecePlacement(ranks[0])) {
+			return STARTING_POSITION_FEN;
+		} else if (!NotationValidator.isValidFEN(fen)) {
+			return ranks[0] + DEFAULT_FEN_EMBELLISH_ARGUMENTS;
+		} else {
+			return fen;
+		}
 	}
 
 	public static char[][] fenToCharArray2d(String piecePlacement) {
--- test/main/java/chess/models/util/FENUtilTest.java
+++ test/main/java/chess/models/util/FENUtilTest.java
@@ -3,9 +3,14 @@ package main.java.chess.models.util;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class FENUtilTest {
 
@@ -15,6 +20,28 @@ class FENUtilTest {
 
 	@AfterEach
 	void tearDown() throws Exception {
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideParametersForRefreshFEN")
+	void testIsValid(String fen, String expected) {
+		assertEquals(expected, FENUtil.refreshFEN(fen));
+	}
+
+	private static Stream<Arguments> provideParametersForRefreshFEN() {
+		return Stream.of(
+				Arguments.of("rnbq1k1r/pp3p1p/6pP/2pNP3/2p3n1/5N2/PP1Q1PP1 w KQkq - 0 1",
+						"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"),
+				Arguments.of("rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2",
+						"rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"),
+				Arguments.of("rnbq1k1r/pp3p1p/6pP/2pNP3/2p3n1/5N2/PP1Q1PP1/R3KB1Q b K - 0 1",
+						"rnbq1k1r/pp3p1p/6pP/2pNP3/2p3n1/5N2/PP1Q1PP1/R3KB1Q w - - 0 1"),
+				Arguments.of("rnbqkbnr/p1p2ppp/1p2p3/1pPp4/8/P7/1P1PPPPP/RNBQKBNR w KQkq b6 0 2",
+						"rnbqkbnr/p1p2ppp/1p2p3/1pPp4/8/P7/1P1PPPPP/RNBQKBNR w - - 0 1"),
+				Arguments.of("rnb1kbnr/pp1q1p1p/2p1p3/2Pp1PpP/8/P7/1P1PP1P1/RNBQKBNR w KQkq g6 -1 6",
+						"rnb1kbnr/pp1q1p1p/2p1p3/2Pp1PpP/8/P7/1P1PP1P1/RNBQKBNR w - - 0 1"),
+				Arguments.of("rnb1kbnr/pp1q1p1p/2p1p3/2Pp1PpP/8/P7/1P1PP1P1/RNBQKBNR w KQkq g6 0 -5",
+						"rnb1kbnr/pp1q1p1p/2p1p3/2Pp1PpP/8/P7/1P1PP1P1/RNBQKBNR w - - 0 1"));
 	}
 
 	@Test
