a600af0b4389aea8f0dfd3bc538ede149e5f5a96
Fivenan <74267476+Fivenan@users.noreply.github.com>
2023-09-02 23:34:23 +0200

    castling move

--- src/main/java/chess/models/oop/moves/CastlingMove.java
+++ src/main/java/chess/models/oop/moves/CastlingMove.java
@@ -1,12 +1,25 @@
 package main.java.chess.models.oop.moves;
 
+import main.java.chess.models.oop.OopChessBoard;
 import main.java.chess.models.oop.Tile;
 
 public class CastlingMove extends Move {
 
-	public CastlingMove(Tile start, Tile end) {
+	Tile rookStartTile;
+	Tile rookEndTile;
+
+	Move rookMove;
+
+	public CastlingMove(OopChessBoard b, Tile start, Tile end) {
 		super(start, end);
-		// TODO Auto-generated constructor stub
+		if (end.file - start.file > 0) {
+			rookStartTile = b.getTile(end.rank, 0);
+			rookEndTile = b.getTile(end.rank, end.file + 1);
+		} else {
+			rookStartTile = b.getTile(end.rank, 7);
+			rookEndTile = b.getTile(end.rank, end.file - 1);
+		}
+		rookMove = new NormalMove(rookStartTile, rookEndTile);
 	}
 
 }
