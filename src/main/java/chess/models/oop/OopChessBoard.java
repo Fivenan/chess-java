e2174d51cc9c1ef9a004ffc71c6f086c3e46399a
Fivenan <74267476+Fivenan@users.noreply.github.com>
2023-09-10 22:22:08 +0200

    move FEN update

--- src/main/java/chess/models/oop/OopChessBoard.java
+++ src/main/java/chess/models/oop/OopChessBoard.java
@@ -211,6 +211,8 @@ public class OopChessBoard implements ChessBoard {
 			Tile cdt = cm.getCapturedTile();
 //			Piece capturedPiece = cdt.getPiece(); // for storing purposes, e.g. Shogi-like gameplay
 			cdt.clear();
+		} else {
+			halfmoveClock++;
 		}
 		if (move instanceof CastlingMove) {
 			CastlingMove cm = (CastlingMove) move;
@@ -218,23 +220,24 @@ public class OopChessBoard implements ChessBoard {
 			if ((rt.file == 0 && rt.rank == 0 && blackCanCastleQueenSide)
 					|| (rt.file == 7 && rt.rank == 0 && blackCanCastleKingSide)) {
 				apply(cm.getRookMove());
-				cm.getEnd().setPiece(cm.getStart().getPiece());
-				cm.getStart().clear();
 				blackCanCastleQueenSide = false;
 				blackCanCastleKingSide = false;
 			} else if ((rt.file == 0 && rt.rank == 7 && whiteCanCastleQueenSide)
 					|| (rt.file == 7 && rt.rank == 0 && whiteCanCastleKingSide)) {
 				apply(cm.getRookMove());
-				cm.getEnd().setPiece(cm.getStart().getPiece());
-				cm.getStart().clear();
 				whiteCanCastleQueenSide = false;
 				whiteCanCastleKingSide = false;
 			}
-
 		}
-
 		move.getEnd().setPiece(movingPiece);
 		move.getStart().clear();
+
+		if (turn == Color.BLACK) {
+			fullmoveNumber++;
+			turn = Color.WHITE;
+		} else {
+			turn = Color.BLACK;
+		}
 
 	}
 
