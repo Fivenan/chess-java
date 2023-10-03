bd3a94705ece076ce578c7e48e62b7a87e5694af
Fivenan <74267476+Fivenan@users.noreply.github.com>
2023-10-03 21:39:49 +0200

    board view

--- src/main/java/chess/views/ChessBoardPane.java
+++ src/main/java/chess/views/ChessBoardPane.java
@@ -1,9 +1,9 @@
 package main.java.chess.views;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import main.java.chess.models.enums.Color;
 
 public class ChessBoardPane extends GridPane {
 
@@ -15,16 +15,22 @@ public class ChessBoardPane extends GridPane {
 		double windowWidth = getWidth();
 		double windowHeight = getHeight();
 
+		double tileWidth = windowWidth > windowHeight ? windowHeight / 8 : windowWidth / 8;
+		tileWidth = 50;
+
 		for (int row = 0; row < 8; row++) {
 			for (int col = 0; col < 8; col++) {
 				Button button = new Button();
-				button.setMinSize(windowWidth / 8, windowHeight / 8);
+				button.setMinSize(tileWidth, tileWidth);
 				button.setText("" + row + "" + col);
-				if ((row + col) % 2 == 0) {
-					button.setStyle("-fx-background-color: " + Color.WHITE.getHex() + "; ");
-				} else {
-					button.setStyle("-fx-background-color: " + Color.BLACK.getHex() + "; ");
-				}
+//				if ((row + col) % 2 == 0) {
+//					button.setStyle("-fx-background-color: " + Color.WHITE.getHex() + "; ");
+//				} else {
+//					button.setStyle("-fx-background-color: " + Color.BLACK.getHex() + "; ");
+//				}
+//				if (row == 0 && col == 0) {
+//					button.setText("");
+//				}
 				buttons[row][col] = button;
 
 				// Set up event handler for button clicks
@@ -38,6 +44,18 @@ public class ChessBoardPane extends GridPane {
 				add(buttons[row][col], col, row);
 			}
 		}
+
+		Label firstNameLabel = new Label("From");
+		TextField firstNameField = new TextField();
+		Label lastNameLabel = new Label("To");
+		TextField lastNameField = new TextField();
+
+		// Add elements to the grid pane
+		add(firstNameLabel, 8, 0);
+		add(firstNameField, 9, 0);
+		add(lastNameLabel, 8, 1);
+		add(lastNameField, 9, 1);
+
 //		// Create UI components
 //		display = new TextField();
 //		display.setEditable(false);
