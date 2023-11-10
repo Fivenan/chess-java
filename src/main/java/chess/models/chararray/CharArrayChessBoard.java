6d92add97200d794c8a80b79ea2f91db9a64c084
Fivenan <74267476+Fivenan@users.noreply.github.com>
2023-11-10 21:56:06 +0100

    board char array

--- src/main/java/chess/models/chararray/CharArrayChessBoard.java
+++ src/main/java/chess/models/chararray/CharArrayChessBoard.java
@@ -1,13 +1,25 @@
 package main.java.chess.models.chararray;
 
+import java.util.logging.Logger;
+
 import main.java.chess.exceptions.InvalidMoveException;
 import main.java.chess.models.ChessBoardInterface;
 import main.java.chess.models.oop.moves.Move;
 import main.java.chess.models.util.BoardUtil;
+import main.java.chess.models.util.NotationValidator;
 
 public class CharArrayChessBoard implements ChessBoardInterface {
 
+	private static final Logger logger = Logger.getLogger(CharArrayChessBoard.class.getName());
+
 	private char[][] tiles;
+
+	private boolean isWhiteTurn = true;
+	private String castling = "KQkq";
+	private String enPassantTargetTile;
+
+	private int halfmoveClock;
+	private int fullmoveNumber;
 
 	public CharArrayChessBoard() {
 
@@ -20,8 +32,45 @@ public class CharArrayChessBoard implements ChessBoardInterface {
 
 	@Override
 	public void setBoard(String fen) {
-		// TODO Auto-generated method stub
+		// Validate FEN
+		if (!NotationValidator.isValidFEN(fen)) {
+			logger.warning(fen + " is not a valid FEN.");
+			return;
+		}
 
+		logger.info("Setting board to " + fen);
+
+		String[] parts = fen.split(" ", 6);
+
+		importFENBoardToTiles(parts[0]);
+		isWhiteTurn = parts[1].equals("w");
+		castling = parts[2];
+
+		if (!parts[3].equals("-")) {
+			enPassantTargetTile = parts[3];
+		} else {
+			enPassantTargetTile = null;
+		}
+
+		halfmoveClock = Integer.valueOf(parts[4]);
+		fullmoveNumber = Integer.valueOf(parts[5]);
+
+	}
+
+	private void importFENBoardToTiles(String fenBoard) {
+		String[] ranks = fenBoard.split("/", 8);
+		emptyBoard();
+
+		for (int rank = 0; rank < 8; rank++) {
+			int file = 0;
+			for (char c : ranks[rank].toCharArray()) {
+				if (Character.isDigit(c)) {
+					file += c - '0';
+					continue;
+				}
+				tiles[rank][file++] = c;
+			}
+		}
 	}
 
 	@Override
